10dfb9ecd4b7f2953e71c17b4fbd33dfd62bfde4
Mikko Keinänen <mikko.keinanen@gmail.com>
2024-09-10 23:38:43 +0300

    Initial eframe egui app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "GameCollection",
+        native_options,
+        Box::new(|cc| Ok(Box::new(GameCollectionApp::new(cc)))),
+    )
+}
+
+#[derive(Default)]
+struct GameCollectionApp;
+
+impl GameCollectionApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for GameCollectionApp {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Game Collection");
+        });
+    }
 }
